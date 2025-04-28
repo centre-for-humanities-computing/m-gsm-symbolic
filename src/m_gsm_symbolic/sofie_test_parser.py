954ebf550f8b50de34132284c34b83a82434bf07
SMosegaard <sofie.mosegaard@live.dk>
2025-04-28 16:18:20 +0200

    tried to implement the feedback from the first PR

--- src/m_gsm_symbolic/sofie_test_parser.py
+++ src/m_gsm_symbolic/sofie_test_parser.py
@@ -2,14 +2,18 @@ import json
 import random
 import re
 from dataclasses import dataclass
-from typing import Self
+from typing import Self, Any, Callable
+import logging
 
+logger = logging.getLogger(__name__)
 
-functions = {
-    "range": lambda start, stop, step=1: range(start, stop, step), # step default 1, so it can handle 2 and 3 arguments
-    "sample": lambda items, n = 2: random.sample(items, n), # trying to make the dict handle multiple items 
-    "divides": lambda a, b: a % b == 0,}
+functions = {"range": lambda start, stop, step = 1: range(start, stop, step),
+             "sample": lambda items, n = 2: random.sample(items, n),
+             "divides": lambda a, b: a % b == 0,}
 
+default_replacements = {"names": ["Sofie", "Andrea", "Freja", "Ida", "Clara", "Anna"],
+                        "multiple_ice": [2, 3],
+                        "multi_times": [2, 3],}
 
 def eval_braces(match, num_values):
     """ Helper function to evalulate the expressions inside the {}"""
@@ -19,7 +23,6 @@ def eval_braces(match, num_values):
         return str(result)  # Replace with the result
     except Exception as e:
         return match.group(0)
-
 
 def eval_expressions(text: str, new_values: dict[str, str]) -> str:
     '''
@@ -35,21 +38,15 @@ def eval_expressions(text: str, new_values: dict[str, str]) -> str:
     text = re.sub(r"\{([^,{}][^{}]*)\}", lambda match: eval_braces(match, num_values), text)
     return text
 
+@dataclass  
+class Question:  
+    question: str  
+    answer: str  
+    id_orig: int  
+    id_shuffled: int 
 
 @dataclass
-class GeneratedQuestion:
-    question: str
-    answer: str
-    id_orig: int
-    id_shuffled: int
-
-
-@dataclass
-class AnnotatedQuestion:
-    question: str
-    answer: str
-    id_orig: int
-    id_shuffled: int
+class AnnotatedQuestion(Question):
     question_annotated: str
     answer_annotated: str
 
@@ -67,14 +64,8 @@ class AnnotatedQuestion:
     def default_values(self) -> dict[str, str]:
         """extract default values from both the annotated question and answer"""
         default_values = {}
-
-        # find {var_name, value} pairs
-        regex = r"\{(\w+),(\w+)\}" # any alphanumeric chr
-
-        # hmmm might not be the smartest solution? But I couldnt replace variable values for the 
-        # answer annoted, if I didnt extract the default dict from it? 
+        regex = r"\{(\w+),(\w+)\}" # find {var_name, value} pairs. Any alphanumeric chr
         annotated_question_answer = self.question_annotated + self.answer_annotated
-
         matches = re.findall(regex, annotated_question_answer) # returns a list containing all matches
         for var_name, value in matches:
             default_values[var_name] = value
@@ -86,13 +77,12 @@ class AnnotatedQuestion:
         return re.sub(r"<<.*?>>", "", text)  
     
     def apply_init_rules(self,
-                        names: list[str],
-                        multiple_ice: list[int],
-                        multi_times: list[int],
-                        functions: dict[str, callable]) -> dict[str, str]:
-
+                         replacements: dict[str, Any] = {},
+                         functions: dict[str, Callable] = functions) -> dict[str, str]:
         """apply init rules to generate new variable values"""
+
         new_values = {}
+        combined = {**default_replacements, **replacements, "functions": functions}
 
         for line in self.init.strip().splitlines(): # split str into seperate lines at line break "\n"
             line = line.lstrip("-").strip() # remove the leading "-" character + space
@@ -102,31 +92,25 @@ class AnnotatedQuestion:
 
             if "range" in rule:
                 range_arguments = list(map(int, re.findall(r"\d+", rule))) # find range of values, digit chr
-                if len(range_arguments) == 2: #hmm might not be the most "stable" solution
+                if len(range_arguments) == 2:
                     start, stop = range_arguments
-                    new_values[var] = str(random.choice(functions["range"](start, stop)))
+                    new_values[var] = str(random.choice(combined["functions"]["range"](start, stop)))
                 elif len(range_arguments) == 3:
                     start, stop, step = range_arguments
-                    new_values[var] = str(random.choice(functions["range"](start, stop, step)))
-                
+                    new_values[var] = str(random.choice(combined["functions"]["range"](start, stop, step)))
+
             elif "sample" in rule:
                 match_list = re.search(r"sample\((.+?)\)", rule)
                 sample_expression = match_list.group(1)
-
-                sampled_arguments = eval(f"functions['sample']({sample_expression})", {"names": names,
-                                                                                       "multiple_ice": multiple_ice,
-                                                                                       "multi_times": multi_times,
-                                                                                       "functions": functions})
+                sampled_arguments = eval(f"functions['sample']({sample_expression})", combined)
                 var_arguments = [v.strip() for v in var.split(",")]
 
                 for var_sample, val_sample in zip(var_arguments, sampled_arguments):
-                    print(f"Assigning {var_sample} = {val_sample}") 
+                    logger.info(f"Assigning {var_sample} = {val_sample}") 
                     new_values[var_sample] = val_sample
 
-            #divides????
-            # I dont think it nessecary to define "divides" -cant it just be calulcated automatically in my eval?
-
         return new_values
+
 
     def replace_values(self, new_values: dict[str, str]) -> None:
         """replace default values in the annotated strings with new sampled values from new_values dict"""
@@ -140,86 +124,61 @@ class AnnotatedQuestion:
             self.question_annotated = re.sub(rf"\{{{var}\}}", str(new_value), self.question_annotated)
             self.answer_annotated = re.sub(rf"\{{{var}\}}", str(new_value), self.answer_annotated)
 
-        return self.question_annotated, self.answer_annotated
-    
+        return self.question_annotated, self.answer_annotated    
 
-    # TEST
+
     def generate_question(self,
-                          names: list[str],
-                          multiple_ice: list[int],
-                          multi_times: list[int],
-                          functions: dict[str, callable]) -> GeneratedQuestion:
+                          replacements: dict[str, Any] = {},
+                          functions: dict[str, Callable] = functions) -> Question:
         """
         The function should take the "original question", then sample new values using the apply_init_rules,
         replace the default values using the replace_values function, evaluate any embedded math expressions,
-        and return everything as a GeneratedQuestion dataclass
+        and return everything as Question dataclass
         """
-        new_values = self.apply_init_rules(names, multiple_ice, multi_times, functions)
+        new_values = self.apply_init_rules(replacements, functions)
         q_annotated, a_annotated = self.replace_values(new_values)
         a_final = eval_expressions(a_annotated, new_values)
-        return GeneratedQuestion(question = self.remove_calculations(q_annotated),
-                                 answer = self.remove_calculations(a_final),
-                                 id_orig = self.id_orig,
-                                 id_shuffled = self.id_shuffled)
+        return Question(question = self.remove_calculations(q_annotated),
+                        answer = self.remove_calculations(a_final),
+                        id_orig = self.id_orig,
+                        id_shuffled = self.id_shuffled)
     
 
-if __name__ == "__main__":
-    
-    names = ["Sofie", "Andrea", "Freja", "Ida", "Clara", "Anna"]
-    multiple_ice = [2, 3]
-    multi_times = [2, 3]
 
-    # sample, range, divides
-    json_data_1 = r"""
-    {
-    "question": "Et brød har 24 skiver. Hvis Andrea kan spise 2 skiver om dagen, mens Johan kan spise dobbelt så meget, hvor mange dage holder brødet så?",
-    "answer": "Johan kan spise 2 x 2 = <<2*2=4>>4 skiver om dagen.\nSammen kan Andrea og Johan spise 2 + 4 = <<2+4=6>>6 skiver om dagen.\nSå et brød vil holde i 24/6 = <<24/6=4>>4 dage.\n#### 4",
-    "id_orig": 921,
-    "id_shuffled": 37,
-    "question_annotated": "Et {item,brød} har {n,24} {unit,skiver}. Hvis {name1,Andrea} kan spise {x,2} {unit,skiver} om dagen, mens {name2,Johan} kan spise {mult,dobbelt} så meget, hvor mange dage holder {item,brød}et så?\n\n#init:\n- item = sample([\"pizza\", \"kage\", \"tærte\", \"lasagne\"])\n- unit = sample([\"stykker\", \"portioner\", \"kuverter\"])\n- name1, name2 = sample(names, 2)\n- $n = range(12, 49, 3)\n- $x = range(2, 6)\n- $mult = sample(multiple_ice+multi_times)\n\n#conditions:\n- divides(n, x + x*mult)\n\n#answer: n // (x + x*mult)",
-    "answer_annotated": "{name2} kan spise {x} x {mult} = <<{x}*{mult}={x*mult}>>{x*mult} {unit} om dagen.\nSammen kan {name1} og {name2} spise {x} + {x*mult} = <<{x}+{x*mult}={x+x*mult}>>{x+x*mult} {unit} om dagen.\nSå et {item} vil holde i {n}/{x+x*mult} = <<{n}/{x+x*mult}={n//(x+x*mult)}>>{n//(x+x*mult)} dage.\n#### {n//(x+x*mult)}"
-    }"""
+def main(json_input):
 
-    # sample, range
-    json_data_2 = r"""
-    {
-    "question": "Der er i øjeblikket 16 år mellem Mia og Emma. Hvis Mia, der er yngre end Emma, er 40 år gammel, hvad er gennemsnittet af deres alder?",
-    "answer": "Hvis Mia er 40 år, er Emma 40+16 = <<40+16=56>>56 år.\nSummen af deres alder er 56+40 = <<56+40=96>>96 år\nGennemsnitsalderen for de to er 96/2 = <<96/2=48>>48 år\n#### 48",
-    "id_orig": 1277,
-    "id_shuffled": 31,
-    "question_annotated": "Der er i øjeblikket {age_diff,16} år mellem {name1,Mia} og {name2,Emma}. Hvis {name1,Mia}, wder er yngre end {name2,Emma}, er {age1,40} år gammel, hvad er gennemsnittet af deres alder?\n\n#init:\n- name1, name2 = sample(names, 2)\n- $age_diff = range(5, 30)\n- $age1 = range(15, 75)\n\n#conditions:\n- is_int((2*age1 + age_diff) / 2)\n\n#answer: (2*age1 + age_diff) // 2",
-    "answer_annotated": "Hvis {name1} er {age1} år gammel, er {name2} {age1}+{age_diff} = <<{age1}+{age_diff}={age1+age_diff}>>{age1+age_diff} år.\nSummen af deres alder er {age1+age_diff}+{age1} = <<{age1+age_diff}+{age1}={2*age1+age_diff}>>{2*age1+age_diff} år\nGennemsnitsalderen for de to er {2*age1+age_diff}/2 = <<{2*age1+age_diff}/2={(2*age1+age_diff)//2}>>{(2*age1+age_diff)//2} år\n#### {(2*age1+age_diff)//2}"
-    }"""
-
-    # Create the Question object
-    question = AnnotatedQuestion.from_json(json_data_2)
+    # Create the AnnotatedQuestion object
+    question = AnnotatedQuestion.from_json(json_input)
 
     # Print values using the dataclass
-    print(f"Question:\n {question.question}")
-    print(f"\nInit section:\n {question.init}")
-    print(f"\nDefault values: {question.default_values}")
-    print(f"\nAnswer witout calculations:\n {question.remove_calculations(question.answer)}")
+    logger.critical(f"Question:\n {question.question}")
+    logger.critical(f"\nInit section:\n {question.init}")
+    logger.critical(f"\nDefault values: {question.default_values}")
+    #logger.critical(f"\nAnswer witout calculations:\n {question.remove_calculations(question.answer)}")
 
     # Sample new values using the init rules and save in a dict
-    new_values = question.apply_init_rules(names, multiple_ice, multi_times, functions)
-    print(f"New values: {new_values}")
+    new_values = question.apply_init_rules(default_replacements)
+    logger.critical(f"New values: {new_values}")
 
     # replace the default values
     question.replace_values(new_values)
 
     #print("\nUpdated annotated question:")
-    print(question.question_annotated)
+    logger.critical(question.question_annotated)
 
     #print("\nUpdated annotated answer:")
-    print(question.answer_annotated)
+    logger.critical(question.answer_annotated)
 
     # calculate result
     question.answer_annotated = eval_expressions(question.answer_annotated, new_values)
-    print(question.answer_annotated)
+    logger.critical(question.answer_annotated)
 
     ## TEST - generate question function
-    print("\n\nTEST, generated question func")
+    logger.critical("\n\nTEST, generated question func")
 
-    generated_question = question.generate_question(names, multiple_ice, multi_times, functions)
-    print(f"Generated question: {generated_question.question}")
-    print(f"Generated answer: {generated_question.answer}")
+    generated_question = question.generate_question(default_replacements)
+    logger.critical(f"Generated question: {generated_question.question}")
+    logger.critical(f"Generated answer: {generated_question.answer}")
+
+if __name__ == "__main__":
+    main(json_input)
