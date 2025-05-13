bd5b6f18c7b3942ef4530ddcb303ca5264ffac2e
SMosegaard <sofie.mosegaard@live.dk>
2025-05-13 18:12:12 +0200

    changed from_json and to_json to take path input

--- src/scripts/gsm_variation.py
+++ src/scripts/gsm_variation.py
@@ -1,17 +1,11 @@
-import os
-import json
-from dataclasses import asdict
 import argparse
 import logging
 from pathlib import Path
 
-os.chdir("..")
 from m_gsm_symbolic.gsm_variation_parser import AnnotatedQuestion
 from m_gsm_symbolic.replacements_list import replacements
 
 logger = logging.getLogger(__name__)
-
-repo_root = Path("./scripts/dan_gsm_variations.py").parent.parent
 
 def parser():
     """ The user can specify how many variations should be generated of one JSON input """
@@ -20,7 +14,7 @@ def parser():
                         "-i",
                         type = str,
                         required = False,
-                        default = "../data/templates/dan/symbolic",
+                        default = "../data/templates/dan/test", # "../data/templates/dan/symbolic",
                         help = "Provide input directory containing JSON data")
     parser.add_argument("--nvariations",
                         "-n",
@@ -28,19 +22,20 @@ def parser():
                         required = False,
                         default = 2,
                         help = "Number of variations to generate per input (return int)")
-    parser.add_argument("--attempts",
-                        "-a",
-                        type = int,
-                        required = False,
-                        default = 5,
-                        help = "Number of attempts to ensure unique variations (return int)")
     args = parser.parse_args()
     return args
+
+
+def create_variants(args):
+    # take args parser argument and make sure it will stay as it was defined
+    # so n_variations fx will stay int
+    return
 
 
 def main():
 
     args = parser()
+    #create_variants(input_dir = args.input_dir, n_variants = args.n_variants) #???
 
     input_dir = Path(args.input_dir)
 
@@ -51,25 +46,16 @@ def main():
     output_dir.mkdir(parents = True, exist_ok = True)
 
     for filename in input_dir.glob("*.json"):
-        with filename.open("r", encoding = "utf-8") as f:
-            json_data = f.read()
-
-    #create_variants(n_variants, attempts, input_dir)
-
-
         for i in range(args.nvariations):
-            
-            question = AnnotatedQuestion.from_json(json_data)
+            question = AnnotatedQuestion.from_json(filename)
 
             input_id = question.id_shuffled
             logger.critical(f"Generating variation number {i+1} for question {input_id}")
 
             generated_question = question.generate_question(replacements)
-            generated_question_dict = asdict(generated_question)
 
-            with open(output_dir / f"{input_id}_{i+1}.json", "w", encoding = 'utf-8') as output_file:
-                json.dump(generated_question_dict, output_file, ensure_ascii = False)
-                logger.critical(f"Saved variation {i+1} for question {input_id} to {output_file.name}")
+            generated_question.to_json(output_dir / f"{input_id}_{i+1}_TESTER.json")
+            logger.critical(f"Saved variation {i+1} for question {input_id} to {output_dir.name}")
 
 
 if __name__ == "__main__":
