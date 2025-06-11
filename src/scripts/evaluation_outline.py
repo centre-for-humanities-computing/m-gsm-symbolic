18330e39f052678520676f7a3330a76e63f2b8b4
Kenneth Enevoldsen <kennethcenevoldsen@gmail.com>
2025-06-11 20:33:29 +0200

    Added evaluation of kænguruen (#31)
    * Updated 0008 eng, tuple logic
    
    * Updated 0017.json eng. Tuple logic.
    
    * Update 0019.json eng. Tuple logic.
    
    * Update 0035.json eng. tuple logic
    
    * Update 0037.json eng. Tuple.
    
    * Update 0041.json eng. Tuple logic
    
    * Update 0043.json eng. Tuple logic
    
    * Update 0044.json eng. Tuple logic
    
    * Update 0047.json eng. Tuple logic
    
    * Update 0053.json eng. Tuple logic
    
    * Update 0055.json eng. Tuple logic
    
    * Update 0057.json eng. Tuple logic
    
    * Update 0058.json eng. Tuple logic
    
    * Update 0059.json eng. Tuple logic
    
    * Update 0061.json eng. Tuple logic
    
    * Update 0069.json eng. Tuple logic
    
    * Update 0079.json eng. Tuple logic
    
    * Update 0086.json eng. Tuple logic
    
    * Update 0088.json eng. Tuple logic
    
    * Update 0089.json eng. Tuple logic
    
    * Update 0091.json eng. Tuple logic
    
    * Update 0092.json eng. Tuple logic
    
    * Update 0093.json eng. Tuple logic
    
    * Update 0094.json eng. Tuple logic
    
    * Update 0095.json eng. Tuple logic
    
    * Update 0096.json eng. Tuple logic
    
    * Update 0097.json eng. Tuple logic
    
    * Added evaluation of kænguruen
    
    The script can be improved, but it outlines the current evaluation
    
    I save it both as a .csv and a .json file
    
    It is not currently possible to load the .json back in (see this [issue](https://github.com/pydantic/pydantic-ai/issues/1953))
    
    Next step is probably to annotate some of the samples to see if the judge is correct.
    
    ---------
    
    Co-authored-by: Sofie Mosegaard <92091425+SMosegaard@users.noreply.github.com>

--- src/scripts/evaluation_outline.py
+++ src/scripts/evaluation_outline.py
@@ -4,57 +4,80 @@ https://ai.pydantic.dev/evals/#installation
 
 """
 
-from importlib.metadata import version
+from pathlib import Path
 
+import pandas as pd
 from pydantic_ai import Agent
 from pydantic_evals import Dataset
 from pydantic_evals.evaluators import LLMJudge
 
 from m_gsm_symbolic.kaenguruen.load_data import load_kaenguruen
 
-print(version("pydantic_evals"))
-
 cases = [p.to_case() for p in load_kaenguruen()]
 
 
-agent_evaluated = Agent("openai:gpt-4.1-nano-2025-04-14")
-# response = agent.run_sync("What is 2+2")
-# print(reponse.output)
-# # '2 + 2 equals 4.'
+judge_llm_name = "openai:gpt-4o-2024-08-06"
+response_model_name = "openai:gpt-4.1-nano-2025-04-14"
+
+agent_evaluated = Agent(response_model_name)
+
 
 evaluator = LLMJudge(
     rubric="The output should be consist expected output, which is the correct answer to the question.",
-    model="openai:gpt-4o",
+    model=judge_llm_name,
     include_input=True,
     include_expected_output=True,
 )
 ds = Dataset(
-    cases=cases[:1],  # simple example with just one case # TODO: remove
+    cases=cases,
     evaluators=[evaluator],
 )
 
 
-# async def answer_question(question: str) -> str:
-#     r = await agent_evaluated.run(question)
-#     r = await answer_extraction.run(question)
-#     return r.output
-
-
-# dummy
 async def answer_question(question: str) -> str:
-    return "I don't know maybe 42?"
+    r = await agent_evaluated.run(question)
+    return r.output
 
+
+root = Path(__file__).parent.parent
+save_path = (
+    root
+    / "data"
+    / "evaluation_results"
+    / "kaenguruen"
+    / f"{response_model_name.replace(':', '__')}.json"
+)
 
 report = ds.evaluate_sync(answer_question)
-report.print(include_input=True, include_output=True, include_durations=True)
+save_path.parent.mkdir(parents=True, exist_ok=True)
+with save_path.open("w") as f:
+    f.write(report.model_dump_json(indent=2))
 
-case_rep = report.cases[0]
-case_rep.output
-case_rep.expected_output
-case_rep.assertions
 
-# Problem:
-# Using the gpt4o model, to evaluate the answer of the gpt-4.1-nano model leads to the answer being correct (even if it is not).
-# I suspect that this is a two way problem:
-# the gpt4o is biased towards the answer of the gpt-4.1-nano model, and
-# the llm judge is badly prompted - we could likely to better with final answer extraction and then comparing if the two outputs are equal using an LLM judge
+# tried reloading the report, but that doesn't work. Added an issue here: https://github.com/pydantic/pydantic-ai/issues/1953
+# from pydantic_evals.reporting import EvaluationReport
+
+# with save_path.open("r") as f:
+#     report = EvaluationReport.model_validate_json(f.read())
+# report = EvaluationReport.model_validate_json(report.model_dump_json(indent=2))
+
+
+# create table from report
+rows = []
+
+for case in report.cases:
+    rows.append(
+        {
+            "name": case.name,
+            "inputs": case.inputs,
+            "expected_output": case.expected_output,
+            "output": case.output,
+            "task_duration": case.task_duration,
+            "assertion_value": case.assertions["LLMJudge"].value,
+            "assertion_reason": case.assertions["LLMJudge"].reason,
+        }
+    )
+df = pd.DataFrame(rows)
+
+# save to csv
+df.to_csv(save_path.with_suffix(".csv"), index=False)
