c91f1f87e22ba19ba4e8f329b23ced0b367675c5
SMosegaard <sofie.mosegaard@live.dk>
2025-06-25 09:21:54 +0200

    file reformatted after ruff format

--- src/scripts/evaluation_outline_gsm.py
+++ src/scripts/evaluation_outline_gsm.py
@@ -1,4 +1,3 @@
-
 from pathlib import Path
 
 import pandas as pd
@@ -15,35 +14,48 @@ response_model_name = "openai:gpt-4.1-nano-2025-04-14"
 
 agent_evaluated = Agent(response_model_name)
 
-evaluator = LLMJudge(rubric = "The output should be consist expected output, which is the correct answer to the question.",
-                     model = judge_llm_name,
-                     include_input = True)
+evaluator = LLMJudge(
+    rubric="The output should be consist expected output, which is the correct answer to the question.",
+    model=judge_llm_name,
+    include_input=True,
+)
 
-ds = Dataset(cases = cases, evaluators = [evaluator],)
+ds = Dataset(
+    cases=cases,
+    evaluators=[evaluator],
+)
+
 
 async def answer_question(question: str) -> str:
     r = await agent_evaluated.run(question)
     return r.output
 
+
 report = ds.evaluate_sync(answer_question)
 
 root = Path(__file__).parent.parent.parent
-save_path = (root
-             / "data"
-             / "evaluation_results"
-             / "gsm_dan"
-             / f"{response_model_name.replace(':', '__')}.json")
+save_path = (
+    root
+    / "data"
+    / "evaluation_results"
+    / "gsm_dan"
+    / f"{response_model_name.replace(':', '__')}.json"
+)
 
 rows = []
 
 for case in report.cases:
-    rows.append({"name": case.name,
-                 "inputs": case.inputs,
-                 "expected_output": case.expected_output,
-                 "output": case.output,
-                 "task_duration": case.task_duration,
-                 "assertion_value": case.assertions["LLMJudge"].value,
-                 "assertion_reason": case.assertions["LLMJudge"].reason,})
+    rows.append(
+        {
+            "name": case.name,
+            "inputs": case.inputs,
+            "expected_output": case.expected_output,
+            "output": case.output,
+            "task_duration": case.task_duration,
+            "assertion_value": case.assertions["LLMJudge"].value,
+            "assertion_reason": case.assertions["LLMJudge"].reason,
+        }
+    )
 
 df = pd.DataFrame(rows)
-df.to_csv(save_path.with_suffix(".csv"), index = False)
+df.to_csv(save_path.with_suffix(".csv"), index=False)
