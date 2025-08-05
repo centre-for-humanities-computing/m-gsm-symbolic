ed08a8dbbd014f597771d708a84101183a602c7b
SMosegaard <sofie.mosegaard@live.dk>
2025-08-05 12:58:45 +0200

    fixed some mistakes and implemented feedback

--- src/scripts/evaluation_1B.py
+++ src/scripts/evaluation_1B.py
@@ -10,7 +10,7 @@ from pydantic_evals.evaluators import LLMJudge
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
 from m_gsm_symbolic.kaenguruen.load_data import load_kaenguruen
-from m_gsm_symbolic.load_data import load_gsm_dan
+from m_gsm_symbolic.load_data import load_gsm_dan, load_gsm_eng
 
 
 if torch.cuda.is_available():
@@ -21,11 +21,23 @@ device = torch.device(dev)
 
 
 class HuggingFaceAgent:
-    def __init__(self, model: str):
+    def __init__(self, model: str, examples: list):
         self.tokenizer = AutoTokenizer.from_pretrained(model)
-        self.model = AutoModelForCausalLM.from_pretrained(model).to(device)
+        self.model = AutoModelForCausalLM.from_pretrained(model, torch_dtype="auto").to(device)
 
-    async def run(self, prompt: str):
+    def _build_prompt(self, prompt):
+        examples = []
+        for case in cases:
+            example = f"Problem: {case.inputs}\n\nSolution: {case.expected_output}"
+            examples.append(example)
+
+        prompt = f"Problem: {prompt}\n\nSolution:"
+        examples.append(prompt)
+        prompt = "\n\n".join(examples)
+        return prompt
+
+    def run(self, prompt: str):
+        prompt = self._build_prompt(prompt)
         model_input = self.tokenizer(prompt, return_tensors="pt").to(device)
 
         model_input.pop("token_type_ids", None)
@@ -33,9 +45,11 @@ class HuggingFaceAgent:
         input_len = model_input["input_ids"].shape[-1]
 
         model_output = self.model.generate(**model_input, max_new_tokens=500)
+        del model_input
         model_output = model_output[0][input_len:]
 
         output_decoded = self.tokenizer.decode(model_output, skip_special_tokens=True)
+        del model_output
 
         return output_decoded
 
@@ -48,7 +62,8 @@ def parser():
         required=True,
         choices=[
             "google/gemma-3-1b-pt",
-            "google/gemma-3-1b-itmeta-llama/Llama-3.2-1B",
+            "google/gemma-3-1b-it",
+            "meta-llama/Llama-3.2-1B",
             "meta-llama/Llama-3.2-1B-Instruct",
             "allenai/OLMo-1B-hf",
             "PleIAs/Pleias-1.2b-Preview",
@@ -69,14 +84,16 @@ def parser():
 def main():
     args = parser()
 
-    judge_llm_name = "openai:gpt-4o-2024-08-06"
-    response_model_name = args.model
-    agent_evaluated = HuggingFaceAgent(response_model_name)
-
     if args.dataset == "kaenguruen":
         cases = [p.to_case() for p in load_kaenguruen()]
     elif args.dataset == "gsm_dan":
         cases = [p.to_case() for p in load_gsm_dan()]
+    elif args.dataset == "gsm_eng":
+        cases = [p.to_case() for p in load_gsm_eng()]
+
+    judge_llm_name = "openai:gpt-4o-2024-08-06"
+    response_model_name = args.model
+    agent_evaluated = HuggingFaceAgent(response_model_name, examples=cases[-3:])
 
     evaluator = LLMJudge(
         rubric="The output should be consist expected output, which is the correct answer to the question.",
@@ -90,7 +107,7 @@ def main():
     )
 
     async def answer_question(question: str) -> str:
-        r = await agent_evaluated.run(question)
+        r = agent_evaluated.run(question)
         return r
 
     report = ds.evaluate_sync(answer_question)
